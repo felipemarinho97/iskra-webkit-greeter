34e086c4f59494e1d84cc57e5c9a24f66aa93322
Marichka Tsiuriak <marichka.ts94@gmail.com>
2018-10-27 11:10:57 -0400

    translate to Ukrainian and Russian

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ import fr from 'react-intl/locale-data/fr'; // French
 import es from 'react-intl/locale-data/es'; // Spanish
 import sv from 'react-intl/locale-data/sv'; // Swedish
 import no from 'react-intl/locale-data/no'; // Norwegian
+import uk from 'react-init/locale-data/uk'; // Ukrainian
+import ru from 'react-init/locale-data/ru'; // Russian
+
 // ... and so on
 
 import { flattenMessages } from './js/utils'; // flatten messages util function in order to use nested js object for translated texts
@@ -21,8 +24,10 @@ require('moment/locale/de');
 require('moment/locale/fr'); 
 require('moment/locale/sv');
 require('moment/locale/no'); 
+require('moment/locale/uk'); 
+require('moment/locale/ru'); 
 
-addLocaleData([...en, ...de, ...fr, ...es, ...sv, ...no]); // don't forget to add here and spread whatever language that was added ex: ...it 
+addLocaleData([...en, ...de, ...fr, ...es, ...sv, ...no, ...uk, ...ru]); // don't forget to add here and spread whatever language that was added ex: ...it 
 
 // constants
 const sDefaultLocale = "en"; // could be changed
